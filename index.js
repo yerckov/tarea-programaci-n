b1d00650da473aa9f6af64a25b2dc1a1070545e0
Chaiguata 1 <yerckovergara33@gmail.com>
2024-06-11 10:40:05 -0400

    tarea programacion

---
+++ index.js
@@ -0,0 +1,28 @@
+//ejercicio con Array//
+
+//ejercicio 1//
+
+let numeros = [1, 2, 3, 4, 5];
+console.log(numeros);
+
+
+//ejercicio 2//
+
+let frutas = ["manzana", "plátano", "uva"];
+console.log(frutas[1]);
+
+//ejercicio 3//
+
+let colores = ["azul", "rojo", "amarillo"];
+colores[2] = "verde";
+console.log(colores);
+
+//ejercicio 4//
+
+let animales = ["perro", "gato", "pájaro"];
+animales.push("conejo");
+console.log(animales);
+
+
+
+
---
+++ index.js2.js
@@ -0,0 +1,39 @@
+//ejercicio con objetos//
+
+//ejercicio 1//
+let persona = {
+    nombre: "yercko" ,
+    edad: 22,
+    ciudad: "castro"
+};
+console.log(persona);
+
+//ejercicio 2//
+
+let coche = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2022
+};
+console.log(coche.modelo);
+
+// ejercicio 3//
+
+let libro = {
+    titulo: "hombre light",
+    autor: "Enrique Rojas"
+};
+
+libro.autor = "George Orwell";
+
+console.log(libro);
+
+// ejercicio 4//
+
+let pelicula = {
+    titulo: "Forrest Gump",
+    director: "Robert Zemeckis"
+};
+pelicula.año = 1994;
+
+console.log(pelicula);
